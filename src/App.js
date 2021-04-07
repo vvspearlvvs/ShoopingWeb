72b39a35171c1bd0f2d60a33f68864e2b9247e89
jinju <gg66477@gmail.com>
2021-04-08 00:18:31 +0900

    add:react frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App () {
+  const [message, setMessage] = useState("ttt");
+  useEffect(() => {
+    fetch('/api/test')
+        .then(response => response.text())
+        .then(message => {
+          setMessage(message);
+        });
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">{message}</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+  )
 }
-
 export default App;
